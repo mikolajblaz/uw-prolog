8e922caaab5e29138c995d2d6828c2b982821b99
Mikołaj Błaż <mb346862@students.mimuw.edu.pl>
2016-05-25 13:31:43 +0200

    initState ready

--- mb346862.pl
+++ mb346862.pl
@@ -6,29 +6,69 @@
 
 ?- ensure_loaded(library(lists)).
 
+
+% replace0(+N, +List, +Elem, -NewList), jeśli NewList to lista List
+% z N-tym elementem zamienionym na Elem (indeksowanie od 0)
+replace0(N, L, E, NewL) :-
+	nth0(N, L, _, Rest),			% usuwamy N-ty element
+	nth0(N, NewL, E, Rest).		% wstawiamy Elem jako N-ty element
+
+
 % Pomocnicze predykaty
 vrb(V) :- vars(VS), member(V, VS).
 arr(A) :- arrays(AS), member(A, AS).
 instr(I, Instr) :- program(P), nth1(I, P, Instr).
 
-
-% Stan jest reprezentowany jako:
-% - lista zmiennych i ich wartości ([zmienna:wartość])
-% - lista tablic i ich wartości ([nazwaTablicy:[indeks:wartość]])
-% - lista instrukcji aktualnie wykonywanych przez kolejne procesy ([proces-instrukcja])
-
-% initProcesses(+N, -CountersList) :- CountersList jest N-elementową listą
-% termów postaci "i:1", gdzie i to indeks elementu w liście (oraz pid procesu)
-initProcesses(N, CountersList) :-
-	integer(N),
-	N >= 1,
-	initProcesses(N, 0, CountersList).
-% TODO: indeksowanie od 0 czy 1
-initProcesses(N, N, []).
-initProcesses(N, PID, [PID:1 | Counters]) :-
-	PID < N,
-	NewPID is PID+1,
-	initProcesses(N, NewPID, Counters).
+readProgram(program(V, A, I)) :-
+	vars(V), arrays(A), program(I).
 
 
-% initState(+N, +Program, -InitState).
+% Stan wykonania programu jest reprezentowany jako term
+%   state(VariableValues, ArrayValues, CounterValues)
+% gdzie:
+%   VariableValues to lista zmiennych i ich wartości:
+%     [Var1-Val1, ..., VarM-ValM]
+%   ArrayValues to lista tablic i ich wartości (a wartość jest listą liczb):
+%     [Arr1-[A1_0, ..., A1_N], ..., ArrM-[AM_1, ..., AM_N]]
+%   CounterValues to lista liczników instrukcji aktualnie wykonywanych
+%   przez kolejne procesy:
+%     [Pid1-C1, ..., PidN-CN]
+
+
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%% Inicjalizacja stanu %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% createConstList(+N, +Val, -List), jeśli List jest
+% N-elementową listą elementów Val
+createConstList(0, _, []).
+createConstList(N, Val, [Val|Tail]) :-
+	N > 0,
+	M is N-1,
+	createConstList(M, Val, Tail).
+
+% createAscendingList(+M, +N, -List), jeśli List jest listą [M, ..., N-1]
+createAscendingList(N, N, []).
+createAscendingList(M, N, [M|Tail]) :-
+	M < N,
+	M1 is M+1,
+	createAscendingList(M1, N, Tail).
+
+% initConstKeys(+Keys, +Val, -List), jeśli Keys jest listą [K1, ..., Kn],
+% zaś List jest listą [K1-Val, ..., Kn-Val]
+initConstKeys([], _, []).
+initConstKeys([K|KS], Val, [K-Val|Tail]) :-
+	initConstKeys(KS, Val, Tail).
+
+
+% initState(+N, +Program, -InitState), jeśli InitState jest
+% stanem początkowym programu Program wykonywanego przez N procesów
+initState(N, program(Vars, Arrs, _), state(VarVals, ArrVals, CVals)) :-
+	createConstList(N, 0, Zeros),
+	createAscendingList(0, N, Pids),			% identyfikatory procesów [0..N-1]
+	initConstKeys(Vars, 0, VarVals),	    % inicjalizujemy zmienne na 0
+	initConstKeys(Arrs, Zeros, ArrVals),	% inicjalizujemy tablice na 0
+	initConstKeys(Pids, 1, CVals).  			% inicjalizujemy liczniki na 1
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%  %%%%%%%%%%%%%%%%%%%%%%%%%%%%
